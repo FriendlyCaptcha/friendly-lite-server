50050941eba5b1999e206bc5bf52f3e1fb3ac9d6
Stefan Laner <laner@codire.de>
2023-04-20 09:15:47 +0200

    Moved classes to src folder

--- public/vendor/autoload.php
+++ public/vendor/autoload.php
@@ -1,5 +1,5 @@
 <?php
-set_include_path(get_include_path().PATH_SEPARATOR.'classes');
+set_include_path(get_include_path().PATH_SEPARATOR.'../src');
 spl_autoload_extensions('.php');
 spl_autoload_register(function(string $className) {
     include str_replace('\\', '/', $className) . '.php';
